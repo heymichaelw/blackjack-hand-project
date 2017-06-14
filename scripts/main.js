277f5b0c51dda4dae9bb4e0bf685bdebfe53532e
Michael <heymichaelw@gmail.com>
2017-06-14 13:16:26 -0400

    passing all tests

--- scripts/main.js
+++ scripts/main.js
@@ -12,40 +12,19 @@
 
 function handValue (hand) {
   var total = 0;
-  var aceindexes = [];
-  var newhand = [];
-  var runningtotal = 0;
   for (var i = 0; i < hand.length; i++) {
     if (hand[i] === 'K' || hand[i] === 'Q' || hand[i] === 'J') {
-      hand[i] = '10';
-    } else if (hand[i] === 'A') {
-      for (var t = 0; t < hand.length; t++) {
-        if (hand[t] === 'A') {
-          aceindexes.push(t);
-        }
+        hand[i] = '10';
       }
-    for (var x = 0; x < aceindexes.length; x++) {
-      newhand = hand.splice((aceindexes[x]), 1);
+    if (hand[i] === 'A'){
+      if (hand.length > 2) {
+        hand[i] = '1';
+      } else{
+        hand[i] = '11';
+      }
     }
-    for (var y = 0; y < newhand.length; y++) {
-      if (newhand[y] === 'K' || newhand[y] === 'Q' || newhand[y] === 'J') {
-        newhand[y] = '10';
-    }
-        runningtotal = parseInt(newhand[y] + runningtotal);
-    }
-    if (runningtotal < 11) {
-      hand[i] = 11;
-    } else if (2 < runningtotal < 10 && aceindexes.length === 1) {
-      hand[i] = 11;
-    } else if (runningtotal >= 20 && aceindexes.length === 1) {
-      hand[i] = 1;
-    } else if (runningtotal <= 1 && aceindexes.length > 1){
-      hand[i] = 1;
-    }
-
-    }
-    total = parseInt(hand[i]) + total;
-  }
+  total = parseInt(hand[i]) + total;
+}
 
 
 
